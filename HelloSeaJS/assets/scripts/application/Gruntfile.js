f1c2c4fceb10cb874ab87755079135a2701ea079
刘奇峰 <qfliu@bainainfo.com>
2013-04-17 15:50:26 +0800

    Add grunt

---
+++ HelloSeaJS/assets/scripts/application/Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt){
+
+	grunt.initConfig({
+		transport : {
+			options : {
+				format : 'hello/dist/{{filename}}'
+			},
+			application : {
+				files : {
+					'.build' : ['application.js','util.js']
+				}
+			}
+		},
+		concat : {
+			main : {
+				options : {
+					relative : true
+				},
+				files : {
+					'dist/application.js' : ['.build/application.js'],
+					'dist/application-debug.js' : ['.build/application-debug.js']
+				}
+			}
+		},
+		uglify : {
+			main : {
+				files : {
+					'dist/application.js' : ['dist/application.js']
+				}
+			}
+		},
+		clean : {
+			build : ['.build']
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-cmd-transport');
+	grunt.loadNpmTasks('grunt-cmd-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	grunt.registerTask('build',['transport','concat','uglify','clean'])
+};
