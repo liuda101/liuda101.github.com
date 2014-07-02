623cab2791d790429f9c4235ea37cbd2a4cc6028
刘奇峰 <liu.da101@gmail.com>
2014-07-02 17:17:53 +0800

    添加长按动画，删掉home button的outline

--- js/app2.js
+++ js/app2.js
@@ -9,6 +9,33 @@
 			this.lockedScreen = new LockedScreen({
 				application : this
 			});
+
+			var self = this;
+
+			this.appMouseDownTimeout = null;
+
+			$('.app').on('mousedown',function(e){
+				window.clearTimeout(self.appMouseDownTimeout);
+				if(!$('.app').hasClass('rotating')){
+					self.appMouseDownTimeout = window.setTimeout(function(){
+						$('.app').each(function(){
+							$(this).addClass('rotating');
+							$(this).addClass('rotating' + Math.ceil(Math.random() * 10 % 4));
+						})
+					},600);
+				}
+			});
+			$('.app').on('mouseup',function(){
+				if(!$('.app').hasClass('rotating')){
+					window.clearTimeout(self.appMouseDownTimeout);
+				}
+			});
+
+			$('#home button').on('click',function(){
+				if($('.app').hasClass('rotating')){
+					$('.app').removeClass('rotating');
+				}
+			});
 		},
 		goToMainScreen : function(){
 			$('html').removeClass('lock');
