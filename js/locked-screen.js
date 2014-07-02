22400f2ca5dc45b58bae11f0198048c0bce1fa05
刘奇峰 <liu.da101@gmail.com>
2014-07-02 17:11:13 +0800

    添加实时日期

--- js/locked-screen.js
+++ js/locked-screen.js
@@ -1,4 +1,6 @@
 (function(){
+	var WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+	var MONTH = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augest', 'September', 'October', 'November', 'December'];
 	var LockedScreen = function(options){
 		this.init(options);
 	};
@@ -12,6 +14,9 @@
 			self.screenDOM = $('#lock-screen');
 			self.slideTip = $('#lock-screen-slide-tip');
 
+			self.timeDetail = $('#lock-screen-time-time');
+			self.dayDetail = $('#lock-screen-time-date');
+
 			self.dragger = $('#lock-screen-slide-btn').draggable({
 				containment: "parent",
 				start : _.bind(self.onDragStart,self),
@@ -20,6 +25,25 @@
 			});
 
 			self.soundEffect = $('#unlock_sound_effect')[0];
+
+			window.setInterval(function(){
+				self.fireTick();
+			}, 60 * 1000);
+
+			self.fireTick();
+		},
+		fireTick: function(){
+			var self = this;
+
+			var date = new Date;
+
+			var timeDetailHTML = (date.getHours() > 9 ? date.getHours() : '0' + date.getHours()) + ':' + (date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes());
+			self.timeDetail.html(timeDetailHTML);
+
+			var dayDetailHTML = WEEK[date.getDay()] + ',' + MONTH[date.getMonth()] + ' ' + date.getDate();
+			self.dayDetail.html(dayDetailHTML);
+
+
 		},
 		onDragStart : function(e,ui){
 			this.slideTip.removeClass('blind');
