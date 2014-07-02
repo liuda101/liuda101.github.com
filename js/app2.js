3452a93407f89c2bb52c9978b641264a0649fa27
catee <452416115@qq.com>
2014-07-02 19:53:48 +0800

    修改时间bug

--- js/app2.js
+++ js/app2.js
@@ -38,16 +38,21 @@
 			});
 		},
 		goToMainScreen : function(){
+			var mm = 60;
 			var timer = setTimeout(function () {
-				var time = new Date();
-				var h = time.getHours() - 1,
-					m = time.getMinutes();
-				var hd = h >= 12 ? 'PM' : 'AM';
-				h = h > 12 ? h - 11 : h;
-				h = h.toString().length === 1 ? '0' + h : '' + h;
-				m = m.toString().length === 1 ? '0' + m : '' + m;
-				$('#time-status').text(h + ' : ' + m + ' ' + hd);
-				timer = setTimeout(arguments.callee, 60000);
+				var date = new Date();
+				var h = date.getHours(),
+					m = date.getMinutes();
+					// s = date.getSeconds();
+				if (m !== mm) {
+					var hd = h >= 12 ? 'PM' : 'AM';
+					h = h > 12 ? h - 12 : h;
+					h = h.toString().length === 1 ? '0' + h : '' + h;
+					m = m.toString().length === 1 ? '0' + m : '' + m;
+					$('#time-status').text(h + ' : ' + m + ' ' + hd);
+					mm = m;
+				}
+				timer = setTimeout(arguments.callee, 1000);
 			}, 0);
 			$('html').removeClass('lock');
 		}
